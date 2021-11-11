ae0b594e6d57520a2ad56f85da81cdce64b3277a
LieneLittlebird <liene.putnina20@gmail.com>
2021-11-11 14:01:37 +0200

    added config for .png and .svg images

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import './styles.css'
+import IMAGE from './react.png'
+import YENOT from './yenot.svg'
+
 const App = () => {
     return (
         <div>
             <h1>
                 If hometask
             </h1>
+            <img src={IMAGE} alt="React logo" width="300" height="200"/>
+            <img src={YENOT} alt="Yenot" width="300"/>
+
         </div>
     )
 }
