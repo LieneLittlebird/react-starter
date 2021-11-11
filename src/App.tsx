6c14fe12c67ce4d174be927769d6714c6051118e
LieneLittlebird <liene.putnina20@gmail.com>
2021-11-11 15:31:51 +0200

    added react refresh plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import './styles.css'
 import IMAGE from './react.png'
 import YENOT from './yenot.svg'
+import Counter from './Counter'
 
 const App = () => {
     return (
         <div>
             <h1>
-                If hometask {process.env.name} 
+                Edited If hometask {process.env.name} 
             </h1>
             <img src={IMAGE} alt="React logo" width="200" height="200"/>
             <img src={YENOT} alt="Yenot" width="300"/>
+            <Counter/> 
         </div>
     )
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,16 @@
 const webpack = require('webpack')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
 
 module.exports = {
     mode: 'development',
     devtool: 'eval-nosources-cheap-module-source-map',
+    devServer: {
+        hot: true,
+        open: true
+      },
     plugins: [
+        new ReactRefreshWebpackPlugin(),
         new webpack.DefinePlugin ({
             'process.env.name': JSON.stringify('Dev lapa')
         })
